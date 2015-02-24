f02efcc4c654b171435c350e0bdff0b64582fc0c
Alex Cevallos <cevalloa@gmail.com>
2015-02-23 21:12:07 -0500

    Added Double Linked List implementation

--- Ch2_Linked_Lists.rb
+++ Ch2_Linked_Lists.rb
@@ -1,3 +1,4 @@
+#Single LinkedList
 class Node
 
 	attr_accessor :value, :next
@@ -49,8 +50,79 @@ class LinkedList
 	end
 
 end
-
+puts "Start Single Linked List"
 testList = LinkedList.new(4)
 testList.add(6)
 testList.add(8)
 testList.display_all
+
+#Double LinkedList
+class NodeDouble
+	attr_accessor :value, :next, :previous
+
+	def initialize(value, next_value = nil, previous_value = nil)
+		@value = value
+		@next = next_value
+		@previous = previous_value
+	end
+
+end
+
+class DoubleLinkedList
+
+	attr_accessor :root
+
+	def initialize(value)
+
+		@root = NodeDouble.new(value, nil, nil)
+
+	end
+
+	def add(value)
+		if @root.nil?
+
+			@root = NodeDouble.new(value, nil,nil)
+
+		else
+
+			current = @root
+			while current.next != nil
+
+				current = current.next
+
+			end
+
+			current.next = NodeDouble.new(value, nil, current)
+
+		end
+
+	end
+
+	def display_all
+
+		puts @root.value
+		current = @root
+
+		while current.next != nil
+			current = current.next
+			puts current.value
+		end
+
+
+	end
+
+end
+
+puts "Starts double linked list"
+testDouble = DoubleLinkedList.new(3)
+testDouble.add(6)
+testDouble.add(8)
+testDouble.display_all
+
+
+
+
+
+
+
+
